f6354fa3bdae483996e3ffa86800787859de9014
Simon Henke <h3.simonh@yahoo.de>
2020-11-16 22:00:28 +0100

    add StrikeThrough alternatives

--- types/TextKey.d.ts
+++ types/TextKey.d.ts
@@ -93,6 +93,8 @@ export interface StrikeThroughEnum {
 export enum StrikeThrough {
   xHeightStrikethroughOn = 'xHeightStrikethroughOn',
   xHeightStrikethroughOff = 'xHeightStrikethroughOff',
+  eMBoxStrikethroughOn = 'eMBoxStrikethroughOn',
+  strikethroughOff = 'strikethroughOff'
 }
 
 export interface UnderlineEnum {
